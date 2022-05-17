c861dd6d49d5e6da8ea5ccad65249e8c10effd69
Rutvik Padhiyar <rutvikpadhiyar141@gmail.com>
2022-05-17 20:15:07 +0530

    near finished

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,8 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    original_url = models.CharField(max_length=500)
+    short_url = models.CharField(max_length=5, unique=True)
--- shortner/views.py
+++ shortner/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from secrets import token_urlsafe
+from .models import URL
 # Create your views here.
+
+def index(request):
+    short = ''
+    if request.method == 'POST':
+        short = token_urlsafe(3)
+        url = request.POST['original']
+        while True:
+            try:
+                url_obj = URL.objects.create(original_url=url, short_url=short)
+            except:
+                continue
+            if url_obj:
+                messages.success(request, 'success')
+                break
+    return render(request, 'shortner/index.html', context={'short': request.get_host() + '/' + short})
+
+
+def redirector(request, short):
+    print(short)
+    url_obj = get_object_or_404(URL, short_url=short)
+    return redirect(url_obj.original_url)
